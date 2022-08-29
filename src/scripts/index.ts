25687476a57bfc21e66ff0e2559e3041d19a09d9
alanfedrizzi <alanfedrizzi@gmail.com>
2022-08-29 18:00:34 -0300

    animation na troca de telas

--- src/scripts/index.ts
+++ src/scripts/index.ts
@@ -1,7 +1,6 @@
 // templarios
-import '../../node_modules/@ionic/core/templarios/css/themes/default.css';
-import '../../node_modules/@ionic/core/templarios/css/themes/medsoft.css';
 import '../../node_modules/@ionic/core/templarios/css/themes/extensivo.css';
+import '../../node_modules/@ionic/core/templarios/css/themes/default.css';
 import '../../node_modules/@ionic/core/templarios/css/schemes/light.css';
 import '../../node_modules/@ionic/core/templarios/css/templarios-css-framework.css';
 
